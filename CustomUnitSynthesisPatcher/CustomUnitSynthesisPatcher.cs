dce69aad64f6547d4be6a14468ba707274435845
Steven Lilley <steven.b.lilley@gmail.com>
2021-07-19 17:40:29 -0400

    Setting up the BaseUnityPlugin mod foundation.

---
+++ CustomUnitSynthesisPatcher/CustomUnitSynthesisPatcher.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Steamworks;
+using System.Collections.Generic;
+using Trainworks.Interfaces;
+using Trainworks.Managers;
+
+namespace CustomUnitSynthesisPatcher
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("MonsterTrain.exe")]
+    [BepInProcess("MtLinkHandler.exe")]
+    [BepInDependency("tools.modding.trainworks")]
+    public class CustomUnitSynthesisPatcher : BaseUnityPlugin, IInitializable
+    {
+        public static CustomUnitSynthesisPatcher Instance { get; private set; }
+
+        public const string GUID = "test.modding.unitsynthesis";
+        public const string NAME = "Custom Unit Synthesis Patcher";
+        public const string VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            Instance = this;
+
+            var harmony = new Harmony(GUID);
+            harmony.PatchAll();
+            Logger.LogInfo($"{NAME} has patched.");
+        }
+
+        public void Initialize()
+        {
+            Logger.LogInfo($"{NAME} initialization began.");
+        }
+
+        public static void Log(string message)
+        {
+            Instance.Logger.LogInfo(message);
+        }
+    }
+
+
+}
